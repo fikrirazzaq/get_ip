6e98e1540230050dd61e22112a9570c3e23b2bd0
Fikri <fikri.arrasyid@fnbees.com>
2021-09-09 11:20:15 +0700

    upgrade plugin to v2 embedding.

--- lib/get_ip.dart
+++ lib/get_ip.dart
@@ -1,4 +1,3 @@
-
 import 'dart:async';
 
 import 'package:flutter/services.dart';
@@ -6,8 +5,21 @@ import 'package:flutter/services.dart';
 class GetIp {
   static const MethodChannel _channel = MethodChannel('get_ip');
 
-  static Future<String?> get platformVersion async {
-    final String? version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+  ///
+  /// Returns the IPv4-Address the device is aware of
+  /// (depending on your network configuration)
+  /// Can be null
+  static Future<String?> get ipAddress async {
+    final String? ip = await _channel.invokeMethod('getIpAdress');
+    return ip;
+  }
+
+  ///
+  /// Returns the users IPv6-Address the device is aware of
+  /// (depending on your network configuration)
+  /// Is null on iOS
+  static Future<String?> get ipv6Address async {
+    final String? ip = await _channel.invokeMethod('getIpV6Adress');
+    return ip;
   }
 }
