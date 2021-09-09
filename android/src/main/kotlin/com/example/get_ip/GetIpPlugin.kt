4895e64264a22a36479753275bc4608fe4b295b8
Fikri <fikri.arrasyid@fnbees.com>
2021-09-09 11:23:04 +0700

    fix: dependencies import.

--- android/src/main/kotlin/com/example/get_ip/GetIpPlugin.kt
+++ android/src/main/kotlin/com/example/get_ip/GetIpPlugin.kt
@@ -7,7 +7,8 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-
+import java.net.NetworkInterface
+import java.util.*
 /** GetIpPlugin */
 class GetIpPlugin: FlutterPlugin, MethodCallHandler {
   /// The MethodChannel that will the communication between Flutter and native Android
